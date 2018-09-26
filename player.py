bddcdf90cf6b73915d8d8a36eb12bee9623594ba
chfgy5@mail.missouri.edu <chfgy5@mail.missouri.edu>
2018-09-26 18:20:15 -0500

    implementing heuristic

--- player.py
+++ player.py
@@ -7,10 +7,39 @@ class Player:
         self.symbol = symbol
 
     def heuristic(self, node):
-        return 1
+        score = 0
+        # Evaluate score for each of the 8 lines (6 rows, 6 columns, 10 diagonals)
+        for y in range(len(node)):
+            score += self.evaluate_line(node[y].tolist())
+        for x in range(len(node[0])):
+            score += self.evaluate_line(node.T[x].tolist())
+
+        return score
+    
+    def evaluate_line(self, row):
+        me_three_row_two_side = 0
+        opp_three_row_two_side = 0
+        me_three_row_one_side = 0
+        opp_three_row_one_side = 0
+        me_open_two = 0
+        opp_open_two = 0
+        if ['',self.symbol,self.symbol,self.symbol,''] in row:
+            me_three_row_two_side += 1
+        elif ['',self.symbol,self.symbol,self.symbol] in row or [self.symbol,self.symbol,self.symbol,''] in row:
+            me_three_row_one_side += 1
+        elif ['',self.symbol,self.symbol] in row or [self.symbol,self.symbol,''] in row:
+            me_open_two += 1
+        
+        if ['','O','O','O',''] in row:
+            opp_three_row_two_side += 1
+        if ['O'] in row:
+            print("hi")
+
+        return score
+
     
     def minimax(self, b):
-        return [0,0]
+        return np.unravel_index(np.argmax(b), b.shape)
 
     def generate_states(self, b):
         height = len(b.state)

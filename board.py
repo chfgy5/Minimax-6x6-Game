5125ba325517585d864f99cb46ddcab6024bedd0
chfgy5@mail.missouri.edu <chfgy5@mail.missouri.edu>
2018-10-01 18:09:11 -0500

    added winner checking

--- board.py
+++ board.py
@@ -8,3 +8,20 @@ class Board:
 
     def isEmpty(self):
         return np.array_equal(self.state, self.empty)
+
+    def wins(self, symbol):
+        board = self.state
+        for i in range(board.shape[0]):
+            for j in range(board.shape[1]):
+                if j < board.shape[1] - 3 and board[i][j] and symbol == board[i][j] == board[i][j + 1] == board[i][j + 2] == \
+                        board[i][j + 3]:
+                    self.winner = symbol
+                if i < board.shape[0] - 3 and board[i][j] and symbol == board[i][j] == board[i + 1][j] == board[i + 2][j] == \
+                        board[i + 3][j]:
+                    self.winner = symbol
+                if i < board.shape[0] - 3 and j < board.shape[1] - 3 and symbol == board[i][j] and board[i][j] == \
+                        board[i + 1][j + 1] == board[i + 2][j + 2] == board[i + 3][j + 3]:
+                    self.winner = symbol
+                if i < board.shape[0] - 3 and j > 3 and board[i][j] and symbol == board[i][j] == board[i + 1][j - 1] == \
+                        board[i + 2][j - 2] == board[i + 3][j - 3]:
+                    self.winner = symbol
--- main.py
+++ main.py
@@ -2,12 +2,18 @@ from player import *
 
 b =  Board()
 player1 = Player(2, 'X', 'O')
-player2 = Player(2, 'O', 'X')
+player2 = Player(4, 'O', 'X')
 
-while(b.winner == ''):
+while(1):
     player1.take_turn(b)
     print(b.state)
+    b.wins(player1.symbol)
+    if(b.winner != ''):
+        break
     player2.take_turn(b)
     print(b.state)
+    b.wins(player2.symbol)
+    if(b.winner != ''):
+        break
 
-print("The winner is {1}" % b.winner)
+print("The winner is " +  b.winner)
